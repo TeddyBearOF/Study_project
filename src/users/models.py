a615220253ea8a1bff7a6bd0d1fc7b764fd2b6c6
Rudislam <rudislamswmiass2004@gmail.com>
2025-10-30 21:33:07 +0500

    moved class User from src/models to src/users/models

--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,17 @@
+import uuid
+import sqlalchemy as sa
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.users import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4())
+    title: Mapped[str] = mapped_column(sa.String())
+    user_profile = relationship('UserProfile', backref='user', uselist=False)
+    orders = relationship('Orders', backref='user')
 
 
 class UserProfile(Base):
