3ec147dc231639f1db538f8af38353cfa4d0f642
Rudislam <rudislamswmiass2004@gmail.com>
2025-10-30 21:11:51 +0500

    Updated model user

--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeMeta, Mapped, declarative_base, mapped_column
+from sqlalchemy.orm import DeclarativeMeta, Mapped, declarative_base, mapped_column, relationship
 import uuid
 
 metadata = sa.MetaData()
@@ -20,3 +20,4 @@ class User(Base):
     __tablename__ = 'user'
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4())
     title: Mapped[str] = mapped_column(sa.String())
+    user_profile = relationship('UserProfile', backref='user', uselist=False)
