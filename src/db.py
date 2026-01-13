a41b2ef75c2a2d2af836e9cb1e01425952763c97
Rudislam <rudislamswmiass2004@gmail.com>
2026-01-14 03:26:51 +0500

    Откатил изменения, потому что нихера не могу разобраться где что было, проще будет переписать. Конкретно в данном комите закончил работу с Асинхронным Движком

--- src/db.py
+++ src/db.py
@@ -1,16 +1,17 @@
-from contextlib import contextmanager, asynccontextmanager
+from contextlib import  asynccontextmanager
+from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
 
 from src.config import Settings
 
 settings = Settings()
-
 engine = create_async_engine(str(settings.postgres_url))
 
 
 @asynccontextmanager
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async_session = async_sessionmaker(
         engine,
         expire_on_commit=False,
