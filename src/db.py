8aed911259bea93ef177121069d8ec443c6ce287
Rudislam <rudislamswmiass2004@gmail.com>
2026-01-16 07:13:48 +0500

    Неправильно реализовал генератор

--- src/db.py
+++ src/db.py
@@ -10,20 +10,13 @@ settings = Settings()
 engine = create_async_engine(str(settings.postgres_url))
 
 
-@asynccontextmanager
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async_session = async_sessionmaker(
-        engine,
-        expire_on_commit=False,
-        class_=AsyncSession
-    )
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
-    async with async_session() as session:
-        try:
-            yield session
-            await session.commit()
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
